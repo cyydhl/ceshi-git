601ebf48aded52e782fd7d810578fe861e56885d
2309636182@qq.com <a4306627>
2020-06-07 13:12:25 +0800

    第一次提交

---
+++ git-demo/src/main/java/com/itcast/gitdemo/HelloController.java
@@ -0,0 +1,12 @@
+package com.itcast.gitdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("info")
+    public String info(){
+        return "Hello Springboot info";
+    }
+}
